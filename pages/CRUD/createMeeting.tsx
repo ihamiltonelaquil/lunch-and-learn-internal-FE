af047a9a71224aa0103c27d5f4cae62461e0bdbd
MalachiHabib <malachi.habib@outlook.com>
2022-12-14 11:01:27 +1030

    delete works

---
+++ pages/CRUD/createMeeting.tsx
@@ -0,0 +1,14 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
+import LandingPage from "../../components/LandingPage";
+import Navbar from "../../components/Navigation/Navbar";
+import MainPage from "../../components/MainPage";
+import MainCard from "../../components/Cards/MainCard";
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import { stat } from "fs";
+import Link from "next/link";
+
+const createMeeting = () => {
+  return <></>;
+};
+
+export default createMeeting;
