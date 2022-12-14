28ab9ab6fdd5d257121a1b23cc243fa476c85207
MalachiHabib <malachi.habib@outlook.com>
2022-12-14 14:38:30 +1030

    create working :D (looks like shit)

--- pages/CRUD/createMeeting.tsx
+++ pages/CRUD/createMeeting.tsx
@@ -8,7 +8,82 @@ import { stat } from "fs";
 import Link from "next/link";
 
 const createMeeting = () => {
-  return <></>;
+  const [resultData, setResultData] = useState<any[]>([]);
+
+  const [creatorName, setCreatorName] = useState("");
+  const [meetingTime, setMeetingTime] = useState("");
+  const [topic, setTopic] = useState("");
+  const [description, setDescription] = useState("");
+  const [linkToSlides, setLinkToSlides] = useState("");
+  const [teamsLink, setTeamsLink] = useState("");
+
+  function saveData() {
+    let data = {
+      creatorName,
+      meetingTime,
+      topic,
+      description,
+      linkToSlides,
+      teamsLink,
+    };
+    fetch("https://localhost:555/api/LunchAndLearn", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => console.warn("resp", response));
+  }
+
+  return (
+    <>
+      <div className="container w-50 mb-3">
+        <Link className="btn btn-dark" href="/">
+          Home
+        </Link>
+      </div>
+
+      <div className="container w-50">
+        <form>
+          <div className="container w-50 float-start">
+            <div className="form-group">
+              <input
+                id="testInput"
+                className="form-control"
+                onChange={(e) => setCreatorName(e.target.value)}
+              />
+              <input
+                className="form-control"
+                onChange={(e) => setMeetingTime(e.target.value)}
+              />
+              <input
+                className="form-control"
+                onChange={(e) => setTopic(e.target.value)}
+              />
+              <input
+                className="form-control"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <input
+                className="form-control"
+                onChange={(e) => setLinkToSlides(e.target.value)}
+              />
+              <input
+                className="form-control"
+                onChange={(e) => setTeamsLink(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="input-group-append">
+            <button className="btn btn-dark btn-sm" onClick={saveData}>
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default createMeeting;
