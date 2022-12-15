9bdd6eeb160790cde8b65e84caac4a3691d2d034
MalachiH4540 <MHabib2@raa.com.au>
2022-12-15 14:01:12 +1030

    some updates to the db structure rectified

--- components/Cards/MeetingCard.tsx
+++ components/Cards/MeetingCard.tsx
@@ -1,64 +1,69 @@
-import { convertToDate, dateFormatter, timeFormatter, timeOffset } from "../../lib/dateHelper";
-import React, { useState, useEffect } from 'react';
+import {
+  convertToDate,
+  dateFormatter,
+  timeFormatter,
+  timeOffset,
+} from "../../lib/dateHelper";
+import React, { useState, useEffect } from "react";
 import { StyledCard, StyledMeetingCardButton } from "../styledComponents";
 
 interface MeetingData {
-    meetingID: number;
-    topic: string;
-    meetingStart: string;
-    meetingEnd: string;
-    creatorName: string;
-    description: string;
+  meetingID: number;
+  topic: string;
+  meetingStart: string;
+  meetingEnd: string;
+  creatorName: string;
+  description: string;
 }
 
 const MainCard: React.FC<{ meetingData: MeetingData }> = ({ meetingData }) => {
-    const { meetingID, topic, meetingStart, meetingEnd, creatorName } = meetingData;
+  const { meetingID, topic, meetingStart, meetingEnd, creatorName } =
+    meetingData;
 
-    const start = convertToDate(meetingStart);
-    const end = convertToDate(meetingEnd);
-    var now = new Date();
+  const start = convertToDate(meetingStart);
+  const end = convertToDate(meetingEnd);
+  var now = new Date();
 
-    const [meetingState, setMeetingState] = useState({
-        meetingStatus: timeOffset(now, start, end).meetingStatus,
-        meetingOffset: timeOffset(now, start, end).meetingOffset
-    });
+  const [meetingState, setMeetingState] = useState({
+    meetingStatus: timeOffset(now, start, end).meetingStatus,
+    meetingOffset: timeOffset(now, start, end).meetingOffset,
+  });
 
+  useEffect(() => {
+    setTimeout(() => {
+      now = new Date();
+      var updatedMeetingState = timeOffset(now, start, end);
+      setMeetingState(updatedMeetingState);
+    }, 1000);
+  }, [meetingState]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            now = new Date();
-            var updatedMeetingState = timeOffset(now, start, end);
-            setMeetingState(updatedMeetingState);
-        }, 1000);
-    }, [meetingState]);
+  return (
+    <StyledCard key={meetingID}>
+      <div className="mainContent">
+        <h1>{topic}</h1>
+        <p>Presented by</p>
+        <h3>{creatorName}</h3>
 
+        <p>{meetingState.meetingStatus}</p>
+        <h3>{meetingState.meetingOffset}</h3>
 
-
-    return (
-        <StyledCard key={meetingID}>
-            <div className="mainContent">
-                <h1>{topic}</h1>
-                <p>Presented by</p>
-                <h3>{creatorName}</h3>
-
-                <p>{meetingState.meetingStatus}</p>
-                <h3>{meetingState.meetingOffset}</h3>
-
-                <p>at</p>
-                <h3>
-                    {
-                        (timeFormatter.format(convertToDate(meetingStart))) + "-"
-                        + (timeFormatter.format(convertToDate(meetingEnd))) + " "
-                        + (dateFormatter.format(convertToDate(meetingStart)))
-                    }
-                </h3>
-            </div>
-            <span className="buttons row justify-content-center">
-                <StyledMeetingCardButton>Leave a question for Parthay</StyledMeetingCardButton>
-                <StyledMeetingCardButton>More Information</StyledMeetingCardButton>
-            </span>
-        </StyledCard>
-    )
+        <p>at</p>
+        <h3>
+          {timeFormatter.format(convertToDate(meetingStart)) +
+            "-" +
+            timeFormatter.format(convertToDate(meetingEnd)) +
+            " " +
+            dateFormatter.format(convertToDate(meetingStart))}
+        </h3>
+      </div>
+      <span className="buttons row justify-content-center">
+        <StyledMeetingCardButton>
+          Leave a question for Parthay
+        </StyledMeetingCardButton>
+        <StyledMeetingCardButton>More Information</StyledMeetingCardButton>
+      </span>
+    </StyledCard>
+  );
 };
 
 export default MainCard;
--- pages/cardTest.tsx
+++ pages/cardTest.tsx
@@ -1,16 +1,14 @@
 import MeetingCard from "../components/Cards/MeetingCard";
 
-export default function cardTest(){
-    const data = {
-        meetingID: 1,
-        topic: "TailWind CSS",
-        meetingStart: "2022-12-13T01:00:00",
-        meetingEnd: "2022-12-13T02:00:00",
-        creatorName: "Parthay Bhatt",
-        description: "Test Description"
-    }
-    
-    return(
-        <MeetingCard meetingData={data} />
-    )
+export default function cardTest() {
+  const data = {
+    meetingID: 1,
+    topic: "TailWind CSS",
+    meetingStart: "2022-12-15T11:26:31.0852618",
+    meetingEnd: "2022-12-15T11:26:31.0852618",
+    creatorName: "Parthay Bhatt",
+    description: "Test Description",
+  };
+
+  return <MeetingCard meetingData={data} />;
 }
