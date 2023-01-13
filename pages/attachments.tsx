537d9b747eff1ca9854349ede930351222ff0197
Ismael Hamilton El-Aquil <ismael.b.hamilton@gmail.com>
2023-01-13 13:31:36 +1030

    Attachments edited to display all attachment son blob storage, not only upload

--- pages/attachments.tsx
+++ pages/attachments.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { convertToDate } from "../lib/dateHelper";
 
-export default function Attachments(){
+export default function Attachments() {
 
     const [file, setFile] = useState<File | null>(null)
+    const [attachments, setAttachments] = useState<any[]>([]);
 
     function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
         const fileList = event.target.files;
@@ -10,26 +12,46 @@ export default function Attachments(){
             setFile(fileList[0]);
         }
     }
-    
+
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const data = new FormData();
+        const meetingId = "4a996676-19b6-4e87-3623-08daf1d1fcce";
         if (file) {
             data.append("file", file);
-            const res = await fetch("https://localhost:555/api/LunchAndLearn/upload", {
+            const res = await fetch(`https://localhost:555/api/LunchAndLearn/upload?meetingId=${meetingId}`, {
                 method: "POST",
                 body: data,
             });
         }
-    
+
     }
 
-    return(
-    
-    <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload</button>
-    </form>
-    
+    useEffect(() => {
+        fetch(`https://localhost:555/api/LunchAndLearn/attachments/`)
+            .then((res) => res.json())
+            .then((data) => {
+                setAttachments(data);
+            });
+    }, [attachments]);
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input type="file" onChange={handleFileChange} />
+                <button type="submit">Upload</button>
+            </form>
+            <br></br>
+            <h2>Attachments</h2>
+            {attachments.map((data) => {
+                {
+                    return (
+                        <>
+                            <p>{data.fileName} - <a href={data.publicURI}>Download</a></p>
+                        </>
+                    );
+                }
+            })}
+        </>
     )
 }
