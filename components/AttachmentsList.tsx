e37c66628e4ff55c777788bbc226db6ddd34c73f
MalachiH4540 <MHabib2@raa.com.au>
2023-01-18 12:34:43 +1030

    Minor change to the checking for attachmentData

--- components/AttachmentsList.tsx
+++ components/AttachmentsList.tsx
@@ -1,51 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-interface Attachment{
-    attachmentId: string;
-    fileName: string;
-    blobName: string;
-    publicUri: string;
-    meetingId: string;
-    fileType: string;
-    uploadDate: string;
+interface Attachment {
+  attachmentId: string;
+  fileName: string;
+  blobName: string;
+  publicUri: string;
+  meetingId: string;
+  fileType: string;
+  uploadDate: string;
 }
 
 const AttachmentsList: React.FC<{
-    meetingId: string;
-    }> = ({ meetingId }) => {
+  meetingId: string;
+}> = ({ meetingId }) => {
+  const [attachmentData, setAttachmentData] = useState([]);
 
-    const [attachmentData, setAttachmentData] = useState([]);
+  useEffect(() => {
+    fetch(`https://localhost:555/api/LunchAndLearn/attachments/${meetingId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAttachmentData(data);
+      });
+  }, [attachmentData, meetingId]);
 
-    useEffect(() => {
-        fetch(`https://localhost:555/api/LunchAndLearn/attachments/${meetingId}`)
-            .then((res) => res.json())
-            .then((data) => {
-                setAttachmentData(data);
-            });
-        
-    }, [attachmentData, meetingId]);
-
-    if(attachmentData.length == 0) {
-        return (
-            <div>
-                <h3>No Attachments Found</h3>
-            </div>
-        );
-    }
-    else{
-        return (
-            <div>
-                <h3>Attachments</h3>
-                <ul>
-                    {attachmentData.map((attachment: Attachment) => (
-                        <li key={attachment.attachmentId}>
-                            <a href={attachment.publicUri}>{attachment.fileName}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
-}
+  if (!attachmentData.keys) {
+    return (
+      <div>
+        <h3>No Attachments Found</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Attachments</h3>
+        <ul>
+          {attachmentData.map((attachment: Attachment) => (
+            <li key={attachment.attachmentId}>
+              <a href={attachment.publicUri}>{attachment.fileName}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
 
 export default AttachmentsList;
