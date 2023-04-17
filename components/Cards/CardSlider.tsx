ca858aaffd074ceef1420ca7786dd07b352515ab
Ismael Hamilton El-Aquil <ismael.b.hamilton@gmail.com>
2023-04-18 08:08:45 +0930

    * Added check to first response from API * If this is the first response, shift the slider to the next meeting happening, if there is no meeting in the future, go to the tail

--- components/Cards/CardSlider.tsx
+++ components/Cards/CardSlider.tsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import { LazyLoadTypes } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ExpandedMeetingCard from "./ExpandedMeetingCard";
 import { DarkBG } from "../StyleComponents/styledComponents";
 import { useComponentDidMount } from "../../lib/utils";
@@ -11,19 +11,38 @@ import { useComponentDidMount } from "../../lib/utils";
 interface cardSliderProps {
   didChange: boolean,
 }
+
+interface MeetingData {
+  authID: string;
+  meetingID: string;
+  topic: string;
+  meetingStart: string;
+  meetingEnd: string;
+  creatorName: string;
+  description: string;
+}
+
 const CardSlider: React.FC<cardSliderProps> = (props: cardSliderProps) => {
-  const [cardData, setCardData] = useState<any[]>([]);
+  const [cardData, setCardData] = useState<MeetingData[]>([]);
   const [expandedCardIsVisible, setExpandedCardIsVisible] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [expandedCardData, setExpandedCardData] = useState(cardData[0]);
   const didChange = props.didChange;
   const [didUpdate, setDidUpdate] = useState(false);
+  const sliderRef = useRef<Slider>(null);
+  let [firstLoad, setFirstLoad] = useState(true);
 
   useEffect(() => {
     fetch(process.env.NEXT_PUBLIC_API_ROUTE+`/api/Meeting/`)
       .then((res) => res.json())
       .then((data) => {
         setCardData(data);
+        if (firstLoad) {
+          setTimeout(() => {
+            sliderRef.current?.slickGoTo(findNextMeetingIndex(data), true);
+          }, 1);
+          setFirstLoad(false);
+        }
       });
   }, [currentIndex, didChange, didUpdate]);
 
@@ -48,6 +67,22 @@ useEffect(() => {
       setDidUpdate(!didUpdate);
     }, 1000);
   };
+
+  function findNextMeetingIndex(meetings: MeetingData[]): number {
+    const now = new Date();
+  
+    for (let i = 0; i < meetings.length; i++) {
+      const meetingEnd = new Date(meetings[i].meetingEnd);
+  
+      if (meetingEnd > now) {
+        return i;
+      }
+    }
+  
+    // If no upcoming meetings were found, return tail
+    return meetings.length - 1;
+  }
+  
 
   var settings = {
     accessibility: true,
@@ -90,7 +125,7 @@ useEffect(() => {
           ></DarkBG>
         </>
       )}
-      <Slider {...settings}>
+      <Slider {...settings} ref={sliderRef}>
         {cardData.map((data) => {
           {
             return (
